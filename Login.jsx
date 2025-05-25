5906fd5585c6fa7595041052f7a8ff17f3a700d3
Naitikraj68 <money58322@gmail.com>
2025-05-25 14:50:36 +0530

    Create Login.jsx

---
+++ Login.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../firebase";
+import { useNavigate } from "react-router-dom";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      navigate("/chat");
+    } catch (err) {
+      alert("Login Failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login to Waitsapp</h2>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login
