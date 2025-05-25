2dfa25647c9908020e34be41ed356283b66ada5f
Naitikraj68 <money58322@gmail.com>
2025-05-25 14:51:56 +0530

    Create Register.jsx

---
+++ Register.jsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
+
+const Register = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+      await setDoc(doc(db, "users", res.user.uid), {
+        uid: res.user.uid,
+        email
+      });
+      navigate("/chat");
+    } catch (err) {
+      alert("Registration Failed");
+    }
+  };
+
+  return (
+    <form onSubmit={handleRegister}>
+      <h2>Register for Waitsapp</h2>
+      <input type="email" placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input type="password" placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button type="submit">Register</button>
+    </form>
+  );
+};
+
+export default Register
